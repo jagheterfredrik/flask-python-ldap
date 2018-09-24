4cb48ebec3c50d5f3041bdf20c1a140d7f661417
Fredrik Gustafsson <fredrikg@spotify.com>
2018-09-24 12:14:30 -0400

    bump to 0.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='flask-python-ldap',
-    version='0.1.2',
+    version='0.1.3',
     url = 'https://github.com/jagheterfredrik/flask-python-ldap',
     author = 'Fredrik Gustafsson',
     author_email = 'jagheterfredrik@gmail.com',
